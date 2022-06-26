11145181f993b770500a900bede7cb1aebaa6fc9
Anchal Rathore <76997901+anchal0404@users.noreply.github.com>
2022-06-26 13:08:24 +0530

    D2

---
+++ add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter the a\n");
+    scanf("%d", &a);
+    printf("Eneter the b\n");
+    scanf("%d", &b);
+    printf("The sum is %d" ,a+b);
+    return 0;
+}
---
+++ size1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    printf("%d", sizeof(int));
+    
+    return 0;
+}
+
+//preprocessing-compilation-assembly-linking-loading
+
+//Variable- A name given to memory location  
