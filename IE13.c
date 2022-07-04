545aae59549436fa6a9fe629895d5f58ea73750e
Anchal Rathore <76997901+anchal0404@users.noreply.github.com>
2022-07-04 20:34:00 +0530

    IE

--- IE13.c
+++ IE13.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int main(int argc, char const *argv[])
+int main()
 {
     int amount;
     int note500,note100,note50,note20,note10,note5,note2,note1;
@@ -9,42 +9,42 @@ int main(int argc, char const *argv[])
 
     if (amount>=500)
     {
-        note500==amount/500;
+        note500=amount/500;
         amount-= note500*500;
     }
     if (amount>=100)
     {
-        note100==amount/100;
+        note100=amount/100;
         amount-= note100*100;
     }
     if (amount>=50)
     {
-        note50==amount/50;
+        note50=amount/50;
         amount-= note50*50;
     }
     if (amount>=20)
     {
-        note20==amount/20;
+        note20=amount/20;
         amount-= note20*20;
     }
     if (amount>=10)
     {
-        note10==amount/10;
+        note10=amount/10;
         amount-= note10*10;
     }
     if (amount>=5)
     {
-        note5==amount/5;
+        note5=amount/5;
         amount-= note5*5;
     }
     if (amount>=2)
     {
-        note2==amount/2;
+        note2=amount/2;
         amount-= note2*2;
     }
     if (amount>=1)
     {
-        note1==amount/1;
+        note1=amount;
       
     }
     
---
+++ IE14.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    float sal,TA,DA,GS;
+    printf("Enter Your Salary:\n");
+    scanf("%f",&sal);
+    printf("Basic salary is %f\n",sal);
+    if (sal<=10000)
+    {
+      TA=0.1*sal;
+      printf("TA is %f\n",TA);
+      DA=0.05*sal;
+      printf("DA is %f\n",DA);
+    }
+    else if (sal>=10000)
+    {
+        TA=1.2*sal;
+        printf("TA is %f\n",TA);
+        DA=0.06*sal;
+        printf("DA is %f\n",DA);
+
+    }
+     GS=sal+TA+DA;
+     printf("The gross salary of the employee is %f",GS);
+    
+    return 0;
+}
