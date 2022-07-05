b94494cd8048d5c639a9f6cb5e5d60fd64152a58
Anchal Rathore <76997901+anchal0404@users.noreply.github.com>
2022-07-05 14:54:19 +0530

    Create IE16.c

---
+++ IE16.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int maths,sst,science,english,hindi,sum,percentage;
+    printf("Enter the marks of maths out of 100:\n");
+    scanf("%d",&maths);
+   
+    printf("Enter the marks of sst out of 100:\n");
+    scanf("%d",&sst);
+  
+    printf("Enter the marks of science out of 100:\n");
+    scanf("%d",&science);
+   
+    printf("Enter the marks of english out of 100:\n");
+    scanf("%d",&english);
+    
+    printf("Enter the marks of hindi out of 100:\n");
+    scanf("%d",&hindi);
+   
+
+sum = maths + sst + science + hindi + english;
+percentage = (sum/500)*100;
+
+if (percentage>=60)
+{
+   printf("You have got Grade A");
+}
+else if (percentage>=50)           
+{
+    printf("You have got Grade B");
+}
+else if (percentage>=40)
+{
+    printf("You have got Grade C");
+}
+else if (percentage<40)
+{
+    printf("You have got Grade D");
+}
+
+return 0;
+
+}
