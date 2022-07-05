98b2cf3de48c718daa29feb531b990076ff09cef
Anchal Rathore <76997901+anchal0404@users.noreply.github.com>
2022-07-05 21:02:05 +0530

    Switch

--- switch1.c
+++ switch1.c
@@ -1,13 +1,22 @@
   #include<stdio.h>
-  int main(int argc, char const *argv[])
+  int main()
   {
-    int s= 1;
-    switch(s){
-        case 1:
-        printf("your got a laptop");
-        case 0:
-        printf("better luck next time:");
-    }
+  char sex;
+  printf("Enter your Sex:\n ");
+  scanf("%c",&sex);
+  switch (sex)
+  {
+  case 'F':
+    printf("OTA chennai is assigned to you, Best Wishes!\n");
+    break;
+  
+  case 'M':
+  printf("You can apply for IMA and OTA both!");
+  break;
+  default:
+  printf("You Can't participate in this examination.");
+    
+  }
     return 0;
   }
   
